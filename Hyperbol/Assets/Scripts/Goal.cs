ad0fe427044c4ac03bd5fb086043a77749c0d3d8
Unknown <hugodekruijf@hotmail.com>
2018-12-12 15:38:19 +0100

    Goal effect + stroompjes onder player

--- Hyperbol/Assets/Scripts/Goal.cs
+++ Hyperbol/Assets/Scripts/Goal.cs
@@ -6,15 +6,18 @@ public class Goal : MonoBehaviour
 {
     public Teams team;
     public GameManager gameManager;
+    public GameObject blueGoalPS, redGoalPS;
 
     public void Score()
     {
         if (team == Teams.Red)
         {
+            Instantiate(redGoalPS);
             gameManager.GoalForBlue();
         }
         else if (team == Teams.Blue)
         {
+            Instantiate(blueGoalPS);
             gameManager.GoalForRed();
         }
     }
